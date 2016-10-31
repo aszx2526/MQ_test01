8f7337f94e7a4d308ba08f831b6a7c53481756dd
aszx2526 <tsai2526@hotmail.com>
2016-10-31 11:24:17 +0800

    練習動畫系統
    使用拉線的方式
    後設定布林值
    去做判斷

---
+++ P_MQ_2016 0908-0909Digital Taipei/Assets/onRabbit_test.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class onRabbit_test : MonoBehaviour {
+    public Animator anim;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            anim.SetBool("run", true);
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            anim.SetBool("run", false);
+        }
+    }
+}
